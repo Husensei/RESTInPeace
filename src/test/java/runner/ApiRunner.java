ab1fff9969b54bee1816053f3c4eab113e7c697a
Husensei <husensei.18@gmail.com>
2024-06-05 17:40:26 +0700

    add some changes .gitignore, add plugin

--- src/test/java/runner/ApiRunner.java
+++ src/test/java/runner/ApiRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty"},
+        plugin = {"pretty", "html:reports/cucumber-api-report.html"},
         glue = {"stepDefinition"},
         features = {"src/test/java/features"},
         tags = "@api",
