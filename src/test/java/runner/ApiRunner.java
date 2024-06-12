3c6a8958961ac5defe723b87d7ef8a09a34fe952
Husensei <husensei.18@gmail.com>
2024-06-12 19:54:49 +0700

    fix html reports

--- src/test/java/runner/ApiRunner.java
+++ src/test/java/runner/ApiRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty", "html:reports/cucumber-api-report.html", "json:reports/cucumber-api-report.json"},
+        plugin = {"pretty", "html:reports/index.html", "json:reports/index.json"},
         glue = {"stepDefinition"},
         features = {"src/test/java/features"},
         tags = "@api",
